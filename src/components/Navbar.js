fe747424faccbe8a7d5ac83dd60ee6f96161bcd1
avyudaya <avyudaya7@gmail.com>
2023-03-07 20:42:16 +0545

    can create emp and can send req to be created as org

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,257 @@
-function Navbar () {
-    return <h1>Navbar</h1>
-}
+import {
+  Box,
+  Flex,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  useColorMode,
+  Image,
+  useDisclosure,
+  Text,
+  useColorModeValue,
+  Stack,
+  Heading,
+  useToast,
+} from "@chakra-ui/react";
+import Admin from "../abis/Admin.json";
+import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { useEffect, useState } from "react";
+import { Link as ReactRouterLink } from "react-router-dom";
+import logo from "../assets/logo.png";
+import Searchbar from "./Searchbar";
 
-export default Navbar
+const adminLinks = [
+  {
+    name: "Employees",
+    href: "/",
+  },
+  {
+    name: "Organizations",
+    href: "/organizations",
+  },
+  {
+    name: "Notifications",
+    href: "/notifications",
+  },
+];
+const employeeLinks = [
+  {
+    name: "Profile",
+    href: "/",
+  },
+  {
+    name: "Edit Profile",
+    href: "/edit-profile",
+  },
+  {
+    name: "Notifications",
+    href: "/notifications",
+  },
+];
+const orgLinks = [
+  {
+    name: "Profile",
+    href: "/",
+  },
+  {
+    name: "Notifications",
+    href: "/notifications",
+  },
+];
+const noRoleLinks = [
+  {
+    name: "Sign Up",
+    href: "/",
+  },
+  {
+    name: "Notifications",
+    href: "/notifications",
+  },
+];
+
+const NavLink = ({ name, href }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={"md"}
+    _hover={{
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700"),
+    }}
+    as={ReactRouterLink}
+    to={href}
+  >
+    {name}
+  </Link>
+);
+
+export default function Navbar() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [role, setRole] = useState(-1);
+  const [account, setAccount] = useState("");
+  const toast = useToast();
+
+  const loadBlockChainData = async () => {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    if (accounts) {
+      setAccount(accounts[0]);
+    }
+    const networkId = await web3.eth.net.getId();
+    const AdminData = await Admin.networks[networkId];
+
+    if (AdminData) {
+      const admin = await new web3.eth.Contract(Admin.abi, AdminData.address);
+      const isEmployee = await admin?.methods?.isEmployee(accounts[0]).call();
+      const isOrganization = await admin?.methods
+        ?.isOrganization(accounts[0])
+        .call();
+      const owner = await admin?.methods?.owner().call();
+      var role = -1;
+      if (accounts[0] === owner) {
+        role = 0;
+      } else if (isEmployee) {
+        role = 1;
+      } else if (isOrganization) {
+        role = 2;
+      }
+
+      setRole(role);
+    } else {
+      toast({
+        title: "The Admin Contract does not exist on this network!",
+        status: "error",
+        isClosable: true,
+      });
+    }
+  };
+
+  useEffect(() => {
+    const func = async () => {
+      await loadBlockChainData();
+    };
+    func();
+  }, []);
+
+  const roles = ["Admin", "Employee", "Organization"];
+
+  return (
+    <>
+      <Box
+        bg={useColorModeValue("gray.100", "gray.900")}
+        px={4}
+        position="fixed"
+        w="100%"
+        as="header"
+        backgroundColor={
+          colorMode === "light"
+            ? "rgba(237, 242, 247, 0.7)"
+            : "rgba(23, 25, 35, 0.7)"
+        }
+        zIndex={100}
+        backdropFilter="saturate(180%) blur(5px)"
+      >
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <Flex alignItems={"center"} justifyContent={"space-between"}>
+            <IconButton
+              size={"md"}
+              mr={2}
+              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+              aria-label={"Open Menu"}
+              display={{ md: "none" }}
+              onClick={isOpen ? onClose : onOpen}
+            />
+            <HStack>
+              <Image boxSize="40px" objectFit="cover" src={logo} alt="logo" />
+              <Heading as="h3" size="lg">
+                Skillset
+              </Heading>
+              <Searchbar />
+            </HStack>
+          </Flex>
+
+          <Flex alignItems={"center"}>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {role === 0 &&
+                adminLinks.map((link) => (
+                  <NavLink key={link.name} name={link.name} href={link.href}>
+                    {link.name}
+                  </NavLink>
+                ))}
+
+              {role === 1 &&
+                employeeLinks.map((link) => (
+                  <NavLink key={link.name} name={link.name} href={link.href}>
+                    {link.name}
+                  </NavLink>
+                ))}
+
+              {role === 2 &&
+                orgLinks.map((link) => (
+                  <NavLink key={link.name} name={link.name} href={link.href}>
+                    {link.name}
+                  </NavLink>
+                ))}
+
+              {role === -1 &&
+                noRoleLinks.map((link) => (
+                  <NavLink key={link.name} name={link.name} href={link.href}>
+                    {link.name}
+                  </NavLink>
+                ))}
+            </HStack>
+            <HStack>
+              <Text mx={{ base: 0, md: 4 }} fontSize={"md"} color={"gray.400"}>
+                {roles[role]}
+              </Text>
+              <Button onClick={toggleColorMode}>
+                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              </Button>
+            </HStack>
+          </Flex>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {role === 0 &&
+                adminLinks.map((link) => (
+                  <NavLink key={link.name} name={link.name} href={link.href}>
+                    {link.name}
+                  </NavLink>
+                ))}
+
+              {role === 1 &&
+                employeeLinks.map((link) => (
+                  <NavLink key={link.name} name={link.name} href={link.href}>
+                    {link.name}
+                  </NavLink>
+                ))}
+
+              {role === 2 &&
+                orgLinks.map((link) => (
+                  <NavLink key={link.name} name={link.name} href={link.href}>
+                    {link.name}
+                  </NavLink>
+                ))}
+
+              {role === -1 &&
+                noRoleLinks.map((link) => (
+                  <NavLink key={link.name} name={link.name} href={link.href}>
+                    {link.name}
+                  </NavLink>
+                ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
+}
--- src/pages/NoRole/Profile.js
+++ src/pages/NoRole/Profile.js
@@ -1,4 +1,211 @@
-function Profile(){
-    return <h1>Profile</h1>
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  VStack,
+  Heading,
+  Textarea,
+  Select,
+  Button,
+  useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { messageAdmin } from "../../firebase/api";
+import Admin from "../../abis/Admin.json";
+export default function Profile() {
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [description, setDescription] = useState("");
+  const [role, setRole] = useState("0");
+  const [loading, setLoading] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const isNameError = name === "";
+  const isLocationError = location === "";
+  const isMessageError = message === "";
+  const isDescriptionError = description === "";
+  const isRoleError = role === "0";
+
+  const handleNameChange = (e) => setName(e.target.value);
+  const handleLocationChange = (e) => setLocation(e.target.value);
+  const handleMessageChange = (e) => setMessage(e.target.value);
+  const handleDescriptionChange = (e) => setDescription(e.target.value);
+  const handleRoleChange = (e) => setRole(e.target.value);
+
+  const toast = useToast();
+
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    setSubmitted(true);
+
+    if (
+      isNameError ||
+      isLocationError ||
+      isMessageError ||
+      isDescriptionError ||
+      isRoleError
+    ) {
+      setLoading(false);
+      return;
+    }
+
+    const info = {
+      name: name,
+      description: description,
+      location: location,
+    };
+
+    if (role === "1") {
+      const web3 = window.web3;
+      const accounts = await web3.eth.getAccounts();
+      const networkId = await web3.eth.net.getId();
+      const AdminData = await Admin.networks[networkId];
+
+      if(AdminData){
+        const admin = await new web3.eth.Contract(Admin.abi, AdminData.address);
+        
+        try {
+          await admin.methods.createEmployee(
+            accounts[0],
+            name,
+            location,
+            description
+          ).send({ from: accounts[0] });
+          window.location.reload(true);
+        } catch (err) {
+          toast({
+            title: err.message,
+            status: "error",
+            isClosable: true,
+          });
+        }
+      }
+
+    } else {
+      await messageAdmin(info, message);
+      toast({
+        title: "Message sent to admin.",
+        status: "success",
+        isClosable: true,
+      });
+    }
+
+    setName("");
+    setDescription("");
+    setRole("1");
+    setLocation("");
+    setMessage("");
+    setSubmitted(false);
+    setLoading(false);
+  };
+
+  return (
+    <VStack spacing={10} mx={0} px={0}>
+      <VStack width={{ base: "sm", md: "2xl", sm: "md" }}>
+        <Heading as="h3" variant="md" mt={1.5} mb={6}>
+          Sign Up
+        </Heading>
+
+        <FormControl isRequired isInvalid={isNameError && submitted}>
+          <FormLabel mb={4}>Name</FormLabel>
+          <Input type="text" value={name} onChange={handleNameChange} />
+          {submitted ? (
+            isNameError ? (
+              <FormErrorMessage>Name Required</FormErrorMessage>
+            ) : (
+              <></>
+            )
+          ) : (
+            <></>
+          )}
+        </FormControl>
+
+        <FormControl isRequired isInvalid={isLocationError && submitted}>
+          <FormLabel mb={4}>Location</FormLabel>
+          <Input type="text" value={location} onChange={handleLocationChange} />
+          {submitted ? (
+            isLocationError ? (
+              <FormErrorMessage>Location Required</FormErrorMessage>
+            ) : (
+              <></>
+            )
+          ) : (
+            <></>
+          )}
+        </FormControl>
+
+        <FormControl isRequired isInvalid={isRoleError && submitted}>
+          <FormLabel>Role</FormLabel>
+          <Select
+            placeholder="Select role"
+            defaultValue="1"
+            onChange={handleRoleChange}
+          >
+            <option value="1">Employee</option>
+            <option value="2">Organization</option>
+          </Select>
+          {submitted ? (
+            isRoleError ? (
+              <FormErrorMessage>Role Required</FormErrorMessage>
+            ) : (
+              <></>
+            )
+          ) : (
+            <></>
+          )}
+        </FormControl>
+
+        <FormControl isRequired isInvalid={isDescriptionError && submitted}>
+          <FormLabel mb={4}>Description</FormLabel>
+          <Textarea
+            value={description}
+            onChange={handleDescriptionChange}
+            placeholder="Provide a short description about yourself."
+            size="sm"
+          />
+          {submitted ? (
+            isDescriptionError ? (
+              <FormErrorMessage>Description Required</FormErrorMessage>
+            ) : (
+              <></>
+            )
+          ) : (
+            <></>
+          )}
+        </FormControl>
+
+        <FormControl isRequired isInvalid={isMessageError && submitted}>
+          <FormLabel mb={4}>Message</FormLabel>
+          <Textarea
+            value={message}
+            onChange={handleMessageChange}
+            placeholder="Provide a short message to admin about yourself."
+            size="sm"
+          />
+          {submitted ? (
+            isMessageError ? (
+              <FormErrorMessage>Message Required</FormErrorMessage>
+            ) : (
+              <></>
+            )
+          ) : (
+            <></>
+          )}
+        </FormControl>
+      </VStack>
+      <Button
+        colorScheme="pink"
+        bgGradient="linear(to-r, pink.400, pink.500, pink.600)"
+        color="white"
+        variant="solid"
+        type="submit"
+        onClick={handleSubmit}
+        isLoading={loading}
+      >
+        Sign Up
+      </Button>
+    </VStack>
+  );
 }
-export default Profile
